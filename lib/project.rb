36385f8d180b256c29717032e3dbd32347c9ac1e
Matteo Piccini <gnappo.1@live.it>
2019-11-05 00:43:20 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,10 +1,15 @@
 class Project
   
-  attr_reader :title 
+  attr_reader :title, :backers
   
   def initialize(title)
     @title = title
     @backers = []
+  end
+  
+  def add_backer(backer)
+    backers << backer
+    backer.projects = self
   end
   
 end
